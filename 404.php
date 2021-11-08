6c4752805b21ab256f5616decd3f438aa4003ff6
sebastienscdev <scdeveloppeur@gmail.com>
2021-11-09 00:44:58 +0100

    add style.css and fix language

--- 404.php
+++ 404.php
@@ -7,29 +7,29 @@ Template Name: 404
 <?php get_header(); ?>
 
 <section>
-<!-- start-->
-    <div class="container mt-7">
+	<!-- start-->
+	<div class="container mt-7">
 		<div class="col-md-12">
 			<section class="text-center error-404 not-found mt-7 mb-7">
-					<h4>Oups ! This page doesn't exist.</h4>
-					<p>Find the page you need :</p>
-					<div class="clearfix"></div>
-
-					<form role="search" method="post" class="form form-group" action="<?php echo home_url( '/' ); ?>">
-
-						<div class="u-PaddingLeft10 u-PaddingRight10">
-							<div class="input-group">
-							 <input type="search" required class="form-control" placeholder="Find" value="" name="s" title="Search">
-							 <div class="input-group-btn">
-								 <button class="btn btn-default" type="submit" style="padding: 5px 20px!important; height: 38px;">
-									 ok
-								 </button>
-							 </div>
-						 </div>
+				<h4>Désolé mais cette page n'existe pas.</h4>
+				<p>Trouver la page ici :</p>
+				<div class="clearfix"></div>
+				<form role="search" method="post" class="form form-group" action="<?php echo home_url( '/' ); ?>">
+					<div class="u-PaddingLeft10 u-PaddingRight10">
+						<div class="input-group">
+							<input type="search" required class="form-control" placeholder="Find" value="" name="s"
+								title="Search">
+							<div class="input-group-btn">
+								<button class="btn btn-default" type="submit"
+									style="padding: 5px 20px!important; height: 38px;">
+									ok
+								</button>
+							</div>
 						</div>
-						<div class="clearfix"></div>
-						<input type="hidden" name="search-type" value="general" />
-			 	</form>
+					</div>
+					<div class="clearfix"></div>
+					<input type="hidden" name="search-type" value="general" />
+				</form>
 			</section>
 		</div>
 	</div>
--- content-single.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Template Name: Fiches-artistes
- * Template Post Type: Artistes
- */
-  
- get_header();  ?>
